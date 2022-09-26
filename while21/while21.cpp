5bd34a862b2344a9ec307e90dc985c4a893beaba
Емельян Егоров <emelya.kara@yandex.ru>
2022-09-26 18:17:21 +0500

    проба пера

---
+++ while21/while21.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using  namespace std;
+
+int main()
+{
+    int n;
+    bool b;
+    cin >> n;
+    b = 0;
+    while (n > 0)
+    {
+        if ((n % 10 / 2) != 1)
+        {
+            b = 1;
+            break;
+        }
+        n = n / 10;
+    }
+    cout << b;
+}

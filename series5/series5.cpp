ab90e02c46a8f4da574300ba293a7e26b0e9f70b
DELL <DELL@DESKTOP-JMM3BBS>
2022-09-26 18:05:30 +0500

    Добавьте файлы проекта.

---
+++ series5/series5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double c, s, a;
+    int n;
+    cin >> n;
+    s = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        a = (rand()%100)/10.0;
+        cout << "\n" << a;
+        a = modf(a, &c);
+        cout<<"\n" << c;
+        s = s + c;
+    }
+    cout << "\n" << s;
+    return 0;
+}
---
+++ while 21.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using  namespace std;
+
+int main()
+{
+    int n;
+    bool b;
+    cin>>n;
+    b = 0;
+    while (n > 0)
+    {
+        if ((n % 10 / 2) != 1)
+        {
+            b = 1;
+            break;
+        }
+        n = n / 10;
+    }
+    cout << b;
+}
+
+
